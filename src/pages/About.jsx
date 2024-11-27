879308f84f37cd9f704ff21f422fc473c76213c0
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-27 20:10:03 +0100

    fix text color

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,10 @@
 export default function About() {
 	return (
 		<>
-			<div className='container'>
-				<h1 className='my-4 text-center'>Chi Siamo</h1>
-				<p className='my-4 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-				<p className='my-4 text-center'>Copyright 2024</p>
+			<div className='container text-white my-4 text-center'>
+				<h1 className='my-4'>Chi Siamo</h1>
+				<p className='my-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+				<p className='my-4'>Copyright 2024</p>
 			</div>
 		</>
 	)
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 export default function HomePage() {
 	return (
 		<>
-			<div className='container'>
-				<h1 className='my-4 text-center'>Homepage</h1>
-				<p className=' my-4 text-center'>Benvenuto nel mio blog!</p>
-				<p className='my-4 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+			<div className='container text-center my-4 text-white'>
+				<h1 className='my-4'>Homepage</h1>
+				<p className=' my-4'>Benvenuto nel mio blog!</p>
+				<p className='my-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
 			</div>
 		</>
 	)
