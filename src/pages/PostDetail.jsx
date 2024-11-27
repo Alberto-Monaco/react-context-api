c7a98172e3ef0805626a34e32bbf5fb8cd9fc8af
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-27 21:07:03 +0100

    fix card detail

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -15,25 +15,30 @@ export default function PostDetail() {
 	}, [slug])
 
 	return (
-		<>
-			<div className='container'>
-				<div className='column '>
-					<div className='col-md-6'>
-						<div className='card'>
-							<div className='card-title'>
-								<h1 className='text-center'>{post?.title}</h1>
+		<div className='container mt-4'>
+			<div className='row'>
+				<div className='col'>
+					<div className='card'>
+						<div className='row g-0'>
+							<div className='col-5'>
+								<img
+									src={`http://localhost:3006/imgs/posts/${post?.image}`}
+									alt={post?.title}
+									className='w-100 h-100'
+									style={{ height: '100%', backgroundSize: 'cover' }}
+								/>
 							</div>
-							<div className='card-img text-center'>
-								<img src={`http://localhost:3006/imgs/posts/${post?.image}`} alt={post?.title} width={300} />
-							</div>
-							<div className='card-body'>
-								<div className='mt-4'>{post?.content}</div>
-								<div className='mt-4'>{post?.tags.join(', ')}</div>
+							<div className='col-7'>
+								<div className='card-body'>
+									<h1 className='card-title text-center'>{post?.title}</h1>
+									<p className='card-text mt-4'>{post?.content}</p>
+									<p className='card-text mt-4'>{post?.tags?.join(', ')}</p>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
